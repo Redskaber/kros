0df4a0271ba71bd94882e3733456083771b944f9
redskaber <redskaber@foxmail.com>
2024-02-27 18:36:59 +0800

    01_Testing_Integration Tests_pass

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)] // don't used 'cargo test', selfdefine test framework
-#![test_runner(crate::test_runner)] // pointer test inner function.
+#![test_runner(kros::test_runner)] // pointer test inner function. -> used lib 
 #![reexport_test_harness_main = "test_main"] // handle test find main and no_main 
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
-mod serial;
+use kros::println;
 
 
+// entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World!");
-    // panic!("Some panic message");    // test panic!
 
-
-    // test part
     #[cfg(test)]
     test_main();
 
-
     loop {}
 }
+
 
 /// This function is called on panic.
 // our existing panic handler
@@ -33,62 +29,14 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    kros::test_panic_handler(info)      // used lib.rs 
 }
 
-/// This define custom test framework.
-/// handle test print statement complex output
-#[allow(dead_code)]
-trait Testable {
-    fn run(&self) -> ();
-}
-
-impl <T> Testable for T 
-    where
-        T: Fn(),
-{
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-#[cfg(test)] 
-fn test_runner(tests: &[&dyn Testable]) {  // default find main function execute tests.
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
 
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
 }
-
-/// this impl send msg to qemu virtual shutdown os.
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32); 
-    }
-}
-
--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,23 +1,24 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(kros::test_runner)] // used lib
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use kros::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     test_main();
-
     loop {}
-}
-
-fn test_runner(tests: &[&dyn Fn()]) {
-    unimplemented!();
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    loop {}     // loop
+    kros::test_panic_handler(info); // used lib
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
